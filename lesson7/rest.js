a220f299c253b929717c17a4d6bc182ff89ed892
Воронин Дмитрий Андреевич <VDA@azot.kmr>
2020-06-11 08:22:40 +0700

    HW7

---
+++ lesson7/rest.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const router = require('./router');
+
+mongoose.connect('mongodb://localhost:32768/insta', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const Task = require('./models/task');
+const User = require('./models/user');
+
+app.use('/task', async (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.json({ message: 'No token' });
+    } else {
+        const [type, token] = req.headers.authorization.split(' ');
+
+        jwt.verify(token, 'secret-user-token', (err, payload) => {
+            if (err) {
+                return res.json({ message: 'Wrong token' });
+            }
+
+            req.user = payload;
+
+            next();
+        });
+    }
+});
+
+app.use(router);
+
+app.listen(8888);
+
